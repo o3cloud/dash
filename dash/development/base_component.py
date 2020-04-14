60e72a382c7f0dd3272513c186fb56d83bc83239
Pan Fan <nightsailer@gmail.com>
2020-04-14 11:17:15 +0800

    enable component type

--- dash/development/base_component.py
+++ dash/development/base_component.py
@@ -52,8 +52,8 @@ def is_number(s):
 
 def _check_if_has_indexable_children(item):
     if not hasattr(item, "children") or (
-        not isinstance(item.children, Component)
-        and not isinstance(item.children, (tuple, MutableSequence))
+        not isinstance(item.children, Component) and
+        not isinstance(item.children, (tuple, MutableSequence))
     ):
 
         raise KeyError
@@ -92,51 +92,55 @@ class Component(with_metaclass(ComponentMeta, object)):
                 error_string_prefix = "The `{}.{}` component (version {}){}".format(
                     self._namespace,
                     self._type,
-                    getattr(__import__(self._namespace), "__version__", "unknown"),
-                    ' with the ID "{}"'.format(kwargs["id"]) if "id" in kwargs else "",
+                    getattr(__import__(self._namespace),
+                            "__version__", "unknown"),
+                    ' with the ID "{}"'.format(
+                        kwargs["id"]) if "id" in kwargs else "",
                 )
             except ImportError:
                 # Our tests create mock components with libraries that
                 # aren't importable
                 error_string_prefix = "The `{}` component{}".format(
                     self._type,
-                    ' with the ID "{}"'.format(kwargs["id"]) if "id" in kwargs else "",
+                    ' with the ID "{}"'.format(
+                        kwargs["id"]) if "id" in kwargs else "",
                 )
 
             if not k_in_propnames and not k_in_wildcards:
                 raise TypeError(
                     "{} received an unexpected keyword argument: `{}`".format(
                         error_string_prefix, k
-                    )
-                    + "\nAllowed arguments: {}".format(  # pylint: disable=no-member
+                    ) +
+                    "\nAllowed arguments: {}".format(  # pylint: disable=no-member
                         ", ".join(sorted(self._prop_names))
                     )
                 )
 
-            if k != "children" and isinstance(v, Component):
-                raise TypeError(
-                    error_string_prefix
-                    + " detected a Component for a prop other than `children`\n"
-                    + "Did you forget to wrap multiple `children` in an array?\n"
-                    + "Prop {} has value {}\n".format(k, repr(v))
-                )
+            # if k != "children" and isinstance(v, Component):
+            #     raise TypeError(
+            #         error_string_prefix
+            #         + " detected a Component for a prop other than `children`\n"
+            #         + "Did you forget to wrap multiple `children` in an array?\n"
+            #         + "Prop {} has value {}\n".format(k, repr(v))
+            #     )
 
             if k == "id":
                 if isinstance(v, dict):
                     for id_key, id_val in v.items():
                         if not isinstance(id_key, _strings):
                             raise TypeError(
-                                "dict id keys must be strings,\n"
-                                + "found {!r} in id {!r}".format(id_key, v)
+                                "dict id keys must be strings,\n" +
+                                "found {!r} in id {!r}".format(id_key, v)
                             )
                         if not isinstance(id_val, _strings + (int, float, bool)):
                             raise TypeError(
-                                "dict id values must be strings, numbers or bools,\n"
-                                + "found {!r} in id {!r}".format(id_val, v)
+                                "dict id values must be strings, numbers or bools,\n" +
+                                "found {!r} in id {!r}".format(id_val, v)
                             )
                 elif not isinstance(v, _strings):
                     raise TypeError(
-                        "`id` prop must be a string or dict, not {!r}".format(v)
+                        "`id` prop must be a string or dict, not {!r}".format(
+                            v)
                     )
 
             setattr(self, k, v)
